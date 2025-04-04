dcb433611e904b7a2b8f02ff53c9cdd5cbba144c
HoangDinhKien06 <keniss3478@gmail.com>
2025-04-05 04:36:59 +0700

    🔥 Xoá toàn bộ code cũ

--- Menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-#include <SDL.h>
-#include "BaseOj.h"
-void showMenu(SDL_Window* window, SDL_Renderer* renderer, BaseOj& menuBackground);
-#endif
-
--- VictoryScreen.h
+++
@@ -1,7 +0,0 @@
-#ifndef VICTORY_SCREEN_H_
-#define VICTORY_SCREEN_H_
-#include "CommonFunc.h"
-
-void RenderVictory(SDL_Renderer* renderer);
-#endif
-
